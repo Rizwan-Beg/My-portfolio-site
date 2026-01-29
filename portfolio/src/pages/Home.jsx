665fc3623fd2c4747f5f74f88d1c4d352df899b0
Rizwan <rizwan@Rizwans-MacBook-Air.local>
2026-01-29 16:39:43 +0530

    commit

---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export const Home = () => {
+    return (
+        <div>
+            {/* theme Toggle*/}
+            {/* theme Toggle*/}
+            {/* theme Toggle*/}
+        </div>
+    )
+}
---
+++ portfolio/src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+export const NotFound = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">NotFound</h1>
+        </div>
+    )
+}
